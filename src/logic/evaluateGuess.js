c4969c478e6b79399de128ec487ad6ebe28f9faa
James Irving <james24146@gmail.com>
2026-07-25 17:27:59 +0100

    evalute the users guess logic is complete

---
+++ src/logic/evaluateGuess.js
@@ -0,0 +1,35 @@
+//evaluate whether a guess is correct, has correct letters or is completely wrong
+
+function evaluateGuess (guess, target){
+
+   let result = new Array(5);
+   let targetLetters = target.split("");
+
+   //first pass over the guess word to get exact matches 
+   for (let i = 0; i < 5; i++){
+    if (guess[i] == targetLetters[i]){
+        result[i] = "correct";
+        targetLetters[i] = null; //this letter has been used, stops 2nd pass from using it
+    }
+   }
+
+   //second pass to check for present/absent letters
+   for (let i = 0; i<5; i++){
+    if (result[i] == "correct"){
+        continue;
+    }
+    let matchIndex = targetLetters.indexOf(guess[i])
+    if(matchIndex !== -1){
+        result[i] = "present";
+        targetLetters[matchIndex] = null;
+    }
+    else{
+        result[i] = "absent"
+    }
+   }
+
+   return result
+
+}
+
+console.log(evaluateGuess("LUNAR", "ARRAY"))
